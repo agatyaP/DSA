a77a426c61f47350ea5b482a2b7e17ff0ebca64b
agatyaP <82901989+agatyaP@users.noreply.github.com>
2022-06-16 18:55:16 +0530

    Update 141-linked-list-cycle.java

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -24,3 +24,25 @@ public class Solution {
         return false;
     }
 }
+//for calculating length of cycle
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                Listnode temp=slow;
+                int length =0;
+                do{
+                    temp = temp.next;
+                    length++;
+                }while(temp!=fast);
+                return length;
+            }
+            
+        }
+        return false;
+    }
+}
