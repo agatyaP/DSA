1c3dbbfe599517b07b729d05e4bc4d131cf546b8
agatyaP <82901989+agatyaP@users.noreply.github.com>
2022-06-17 23:47:09 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (79.32%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -11,21 +11,34 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
       
-        if (head == null) {
-            return head;
-        }
-        ListNode prev = null;
-        ListNode present = head;
-        ListNode next = present.next;
+//         if (head == null) {
+//             return head;
+//         }
+//         ListNode prev = null;
+//         ListNode present = head;
+//         ListNode next = present.next;
 
-        while (present != null) {
-            present.next = prev;
-            prev = present;
-            present = next;
-            if (next != null) {
-                next = next.next;
-            }
+//         while (present != null) {
+//             present.next = prev;
+//             prev = present;
+//             present = next;
+//             if (next != null) {
+//                 next = next.next;
+//             }
+//         }
+//         return prev;
+        
+        //GFG Solution
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
         }
-        return prev;
+        head = prev;
+        return head;
     }
 }
