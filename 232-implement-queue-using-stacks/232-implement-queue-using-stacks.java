7283720b42e4582f6b69265c18b6eefc9b5e6843
agatyaP <82901989+agatyaP@users.noreply.github.com>
2022-06-19 18:50:00 +0530

    Time: 0 ms (100.00%), Space: 42 MB (31.15%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -1,42 +1,65 @@
 class MyQueue {
-    Stack<Integer> s1 = new Stack<Integer>();
-    Stack<Integer> s2 = new Stack<Integer>();
+    Stack<Integer> s1 = new Stack<Integer>(); //push stack
+    Stack<Integer> s2 = new Stack<Integer>(); //pop stack
+    int front;
 
     public MyQueue() {
         
     }
     
     public void push(int x) {
+       
         // Move all elements from s1 to s2
-        while (!s1.isEmpty())
-        {
-            s2.push(s1.pop());
-            //s1.pop();
-        }
+//         while (!s1.isEmpty())
+//         {
+//             s2.push(s1.pop());
+//             //s1.pop();
+//         }
  
-        // Push item into s1
+//         // Push item into s1
+//         s1.push(x);
+ 
+//         // Push everything back to s1
+//         while (!s2.isEmpty())
+//         {
+//             s1.push(s2.pop());
+//             //s2.pop();
+//         }
+        if(s1.isEmpty())
+            front=x;
         s1.push(x);
- 
-        // Push everything back to s1
-        while (!s2.isEmpty())
-        {
-            s1.push(s2.pop());
-            //s2.pop();
-        }
+        
+        
+        
+        
     }
     
     public int pop() {
-        int x =s1.peek();
-        s1.pop();
-        return x;
+        // int x =s1.peek();
+        // s1.pop();
+        // return x;
+        
+        if(s2.isEmpty()){
+            //then add s1 items in reverse order
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
     }
     
     public int peek() {
-        return s1.peek();
+        // return s1.peek();
+        if(!s2.isEmpty()){
+            return s2.peek();
+        }
+        return front;
     }
     
     public boolean empty() {
-        return s1.isEmpty();
+        // return s1.isEmpty();
+       return s1.isEmpty() && s2.isEmpty();
+            
     }
 }
 
