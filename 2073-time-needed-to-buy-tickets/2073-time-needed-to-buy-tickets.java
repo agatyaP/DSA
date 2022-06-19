69da0881135d68a3da8319b3bad45578a117e0b6
agatyaP <82901989+agatyaP@users.noreply.github.com>
2022-06-19 13:22:30 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (46.73%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        /*
+        (0-k)-no.of tickets=min(how many tickets they have,how many tickets k has)
+        (k+1-n)-no.of tickets=min(how many tickets they have,how many tickets k has-1 coz after k buys all, we dont have to iterate over k+1)
+        */
+        int res=0;
+        // for(int i=0;i<=k;i++){
+        //     res+= Math.min(tickets[i],tickets[k]);
+        // }
+        // for(int i=k+1;i<tickets.length;i++){
+        //     res+= Math.min(tickets[i],tickets[k]-1);
+        // }
+        // return res;
+        //refactoring
+         for(int i=0;i<tickets.length;i++){
+             if(i<=k)
+                 res+= Math.min(tickets[i],tickets[k]);
+             else
+                 res+= Math.min(tickets[i],tickets[k]-1);
+                 
+                 
+         }
+        return res;
+    }
+}
