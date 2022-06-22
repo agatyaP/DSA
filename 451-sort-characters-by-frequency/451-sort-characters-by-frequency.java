007b3c96d5b509384641f6179e28cb857eb14ac0
agatyaP <82901989+agatyaP@users.noreply.github.com>
2022-06-22 20:22:55 +0530

    Time: 34 ms (31.19%), Space: 48.8 MB (65.00%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,23 +1,42 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) 
-            map.put(c, map.getOrDefault(c, 0) + 1);
+//         Map<Character, Integer> map = new HashMap<>();
+//         for (char c : s.toCharArray()) 
+//             map.put(c, map.getOrDefault(c, 0) + 1);
 						
-        List<Character> [] bucket = new List[s.length() + 1];
-        for (char key : map.keySet()) {
-            int frequency = map.get(key);
-            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
-            bucket[frequency].add(key);
-        }
+//         List<Character> [] bucket = new List[s.length() + 1];
+//         for (char key : map.keySet()) {
+//             int frequency = map.get(key);
+//             if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
+//             bucket[frequency].add(key);
+//         }
 				
-        StringBuilder sb = new StringBuilder();
-        for (int pos = bucket.length - 1; pos >= 0; pos--)
-            if (bucket[pos] != null)
-                for (char c : bucket[pos])
-                    for (int i = 0; i < pos; i++)
-                        sb.append(c);
+//         StringBuilder sb = new StringBuilder();
+//         for (int pos = bucket.length - 1; pos >= 0; pos--)
+//             if (bucket[pos] != null)
+//                 for (char c : bucket[pos])
+//                     for (int i = 0; i < pos; i++)
+//                         sb.append(c);
 
+//         return sb.toString();
+//     }
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        PriorityQueue <Character> maxheap = new PriorityQueue <> ((a,b) -> map.get(b) - map.get(a));
+        maxheap.addAll(map.keySet());
+        
+        StringBuilder sb = new StringBuilder();
+        while(!maxheap.isEmpty()){
+            char current = maxheap.remove();
+            for(int i=0; i<map.get(current); i++){
+                //add the character in the string how many times it occurs, like id e appears 2 times, append ee
+                sb.append(current); 
+            }
+        }
         return sb.toString();
     }
+        
 }
