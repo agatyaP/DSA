5c7ac798e1c8f7f7d078a257029d0f8a6ec33f57
agatyaP <82901989+agatyaP@users.noreply.github.com>
2022-06-23 23:32:54 +0530

    Time: 6 ms (69.09%), Space: 52.2 MB (57.82%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumLength(String s) {
+        // int i = 0, j = s.length() - 1;
+        // while (i < j && s.charAt(i) == s.charAt(j)) {
+        //     char c = s.charAt(i);
+        //     while (i <= j && s.charAt(i) == c) {
+        //         i++;
+        //     }
+        //     while (i <= j && s.charAt(j) == c) {
+        //         j--;
+        //     }
+        // }
+        // return j - i + 1;
+        int i=0, j=s.length()-1;
+        while(i<j&&s.charAt(i)==s.charAt(j)){
+            char c=s.charAt(i);
+            while(i<=j&&s.charAt(i)==c){
+                ++i;
+            }
+            while(i<=j&&s.charAt(j)==c){
+                --j;
+        }
+    }
+        return j-i+1;
+}
+}
